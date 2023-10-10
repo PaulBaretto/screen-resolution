a93e362ba465866745108277f84f9fee095b1d61
Paul Barretto <paulbaretto827@gmail.com>
2023-10-10 15:37:51 +0530

    updated main.js

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 
 
 let wrapper = document.querySelector('.wrapper')
-// let WH = document.querySelector('.W-H');
-let w = window.innerWidth;
-let h = window.innerHeight;
 
 // creating elements 
 let wh_holder = document.createElement('div')
 let h1 = document.createElement('h1');
 let h2 = document.createElement('h2');
 
+
 h1.textContent = 'Your Screen Resolution is';
-h2.textContent = `${w} X ${h}`;
-
-
-
 wrapper.appendChild(wh_holder)
 wh_holder.appendChild(h1)
 wh_holder.appendChild(h2)
 
+
+function updateResolution(){
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    h2.textContent = `${w} X ${h}`;
+}
+
+updateResolution();
+
+window.addEventListener('resize', updateResolution)
